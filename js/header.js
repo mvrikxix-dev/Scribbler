62889c6c31bf91b1d3c61edbf3ecbb2381ac3c7a
mvrikxix <mvrikxix.dev@gmail.com>
2019-07-09 17:06:25 +0530

    Created: Landing Page; Editted: index.html, index.css, header.css, index.js, header.js

---
+++ js/header.js
@@ -0,0 +1,30 @@
+var close = document.getElementsByClassName('cancel');
+var signupModal = document.getElementById('signUp-modal');
+var signinModal = document.getElementById('signIn-modal');
+
+Array.from(close).forEach(function (i) {
+    i.onclick = function () {
+        signupModal.style.display = "none";
+        signinModal.style.display = "none";
+    }
+});
+
+const href = document.getElementsByClassName('href');
+Array.from(href)[0].onclick = function () {
+    signinModal.style.display = "none";
+    signupModal.style.display = "block";
+
+}
+
+const signupButton = document.getElementById('sign-up');
+const signinButton = document.getElementById('sign-in');
+
+signupButton.onclick = function(){
+    signinModal.style.display="none";
+    signupModal.style.display="block";
+}
+
+signinButton.onclick = function(){
+    signupModal.style.display="none"
+    signinModal.style.display="block";
+}
---
+++ js/index.js
@@ -0,0 +1,5 @@
+const allPost = document.getElementById('all-post');
+
+allPost.onclick = function(){
+    window.location.href="/html/bloglist.html";
+}
